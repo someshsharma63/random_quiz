e050b10db1f2770dba68da86fac10c3a89e4b118
Somesh <someshdevera98@gmail.com>
2024-10-23 19:32:53 +0530

    quiz app in js

--- script.js
+++ script.js
@@ -1,46 +1,81 @@
-let form = document.querySelector("form");
 let baseurl="https://opentdb.com/api.php";
-console.log(baseurl)
+let questionsWrapper=document.querySelector(".questionsWrapper");
+let newdata=category.value;
+let form=document.querySelector("form");
 
-let questionsWrapper = document.querySelector(".questionsWrapper");
-// console.log(questionsWrapper)
-
-
-form.addEventListener('submit',createurl);
-
-function createurl(e)
+form.addEventListener('submit',(e)=>
 {
-  e.preventDefault();    //prevent the page from refreshing
-  let formdata = new FormData(form);  //a js function which create form object (data easily server p send kar ske);
- 
-const playerData = Object.fromEntries(formdata);  //key value pair s object ko create karta hai.
-  // console.log(playerData)
+  e.preventDefault()
+  let formdata=new FormData(form);
+let playerData=Object.fromEntries(formdata);
 
-  const actualURL = `${baseurl}?${getQueryString(playerData)}`;//getQueryString url se parameter ko extract karta hai.
-// console.log(actualURL);
+console.log(playerData);
+console.log(formdata);
 
-//const actualURL = `${baseURL}?amount=${playerData.amount}&category=${playerData.category}&difficulty=${playerData.difficulty}&type=${playerData.type}`;
+const actualURL = `${baseurl}?amount=${playerData.amount}&category=${playerData.category}&difficulty=${playerData.difficulty}&type=${playerData.type}`;
+apidata(actualURL)
+console.log(actualURL)
+})
 
-      console.log(actualURL);
-    fetchData(actualURL);
-}
-function getQueryString()
-{
-  let querystring=""
-  for(x in data)
-  {
-    querystring+=
-    querystring.length === 0 ? `${x}=${data[x]}` : `&${x}=${data[x]}`;
-  }
-  return querystring;
-}
 
-function fetchData(url)
-{
+function apidata(url) {
   fetch(url)
-  .then((response) => response.json())
-  .then((result) => {
-      console.log(result.results);
-    showData(result.results);
-  });
+    .then((response) => response.json())
+    .then((result) => {
+        // console.log(result.results);
+      showData(result.results);
+    });
 }
+
+function showData(apidata)
+{
+  const newData=apidata.map((obj)=>{ // each obj will traverse on fetchedData(ie array of objects). 
+    // inside each obj there is an array of incorrect_answers. 
+    obj.incorrect_answers.push(obj.correct_answer)
+    return obj;
+});
+
+  let questiondiv=document.createElement("div");
+  apidata.forEach((obj)=>
+  {
+      let questions=document.createElement("h1");
+      questions.innerHTML=obj.question
+
+    
+      let questionopt=document.createElement("div");
+      for(let i=0;i<4;i++)
+      {
+          let option=document.createElement("p");
+          option.innerHTML=obj.incorrect_answers[i];
+          questionopt.append(option);
+      }
+    
+      questiondiv.append(questions,questionopt);
+      
+
+  })
+
+  questionsWrapper.append(questiondiv);
+ 
+
+}
+
+
+
+
+
+
+
+
+
+
+// console.log(newdata);
+
+// for(let x in baseurl)
+// {
+
+// }
+
+
+
+//amount=10&category=9&difficulty=easy&type=multiple
